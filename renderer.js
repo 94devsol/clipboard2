db2856197ce173000bc7a6baea23176453bf88d3
ar-naseef <naseef14@gmail.com>
2019-02-08 10:41:49 +0530

    click on text to write to clipboard

--- renderer.js
+++ renderer.js
@@ -8,15 +8,37 @@ console.log("from renderer");
 let clipboardHistoryList = [];
 
 clipboardX.on('text-changed', () => {
+
     let clipboardHistoryHTML = '';
     let currentClipboard = clipboardX.readText()
     clipboardHistoryList.push(currentClipboard);
 
-    console.log(clipboardHistoryList)
-    clipboardHistoryList.forEach(item => {
-        clipboardHistoryHTML += `<li>${item}</li>`
+    // console.log(clipboardHistoryList)
+
+    if (clipboardHistoryList.length > 3) {
+        clipboardHistoryList = clipboardHistoryList.splice(1,3);
+    }
+
+    // console.log(clipboardHistoryList)
+    clipboardHistoryList.forEach((item, i) => {
+        clipboardHistoryHTML += `<li id="elem-${i}">${item}</li>`
     })
 
     document.getElementById('current-clipboard').innerHTML = currentClipboard;
     document.getElementById('clipboard').innerHTML = clipboardHistoryHTML;
+    
+    
+    document.getElementById("elem-0").addEventListener("click", (e) => {
+        console.log(e.target.innerText)
+        clipboardX.writeText(e.target.innerText)
+    })
+    document.getElementById("elem-1").addEventListener("click", (e) => {
+        console.log(e.target.innerText)
+        clipboardX.writeText(e.target.innerText)
+    })
+    document.getElementById("elem-2").addEventListener("click", (e) => {
+        console.log(e.target.innerText)
+        clipboardX.writeText(e.target.innerText)
+    })
 }).startWatching();
+
