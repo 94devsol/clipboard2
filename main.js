159a17b91c4bb78caeb0141825260a4a9e280933
sajasnm <sajas@94dev.xyz>
2020-12-25 17:25:57 +0530

    new dark theme ui added

--- main.js
+++ main.js
@@ -1,239 +1,228 @@
 const {
-	app,
-	BrowserWindow,
-	ipcMain,
-	clipboard,
-	Menu,
-	Tray
-} = require('electron');
+  app,
+  BrowserWindow,
+  ipcMain,
+  clipboard,
+  Menu,
+  Tray,
+} = require("electron");
 const randomstring = require("randomstring");
-const ioHook = require('iohook');
-const clipboardWatcher = require('electron-clipboard-watcher');
+const ioHook = require("iohook");
+const clipboardWatcher = require("electron-clipboard-watcher");
 const clipboardMaxLength = 5;
-let win
-let tray = null
+let win;
+let tray = null;
 
 let clipboardHistoryList = [];
 
-
 //clipboardRotate START
 const clipboardRotate = (arr) => {
-	let currentIndex = arr.findIndex((elem) => {
-		return elem.isCurrent == true
-	})
-	console.log("current index" + currentIndex);
-	if (currentIndex === arr.length - 1) {
-		clipboard.writeText(arr[0].value);
-		arr = arr.map(item => {
-			item.isCurrent = false;
-			return item;
-		})
-		arr[0].isCurrent = true
-	} else {
-		clipboard.writeText(arr[currentIndex + 1].value);
-		arr = arr.map(item => {
-			item.isCurrent = false;
-			return item;
-		})
-		arr[currentIndex + 1].isCurrent = true;
-	}
-	return arr;
-}
+  let currentIndex = arr.findIndex((elem) => {
+    return elem.isCurrent == true;
+  });
+  console.log("current index" + currentIndex);
+  if (currentIndex === arr.length - 1) {
+    clipboard.writeText(arr[0].value);
+    arr = arr.map((item) => {
+      item.isCurrent = false;
+      return item;
+    });
+    arr[0].isCurrent = true;
+  } else {
+    clipboard.writeText(arr[currentIndex + 1].value);
+    arr = arr.map((item) => {
+      item.isCurrent = false;
+      return item;
+    });
+    arr[currentIndex + 1].isCurrent = true;
+  }
+  return arr;
+};
 //clipboardRotate END
 
-
 createWindow = () => {
+  win = new BrowserWindow({
+    show: false,
+    frame: false,
+    // titleBarStyle: 'hidden',
+    backgroundColor: "#fff",
+    height: 370,
+    width: 700,
+    // skipTaskbar: true,
+    // resizable: false,
+    // maxHeight: 190,
+    // width: 250,
+    // maxWidth: 510,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: true,
+    //   minimumFontSize: 16,
+    //   defaultFontSize: 18,
+      // defaultMonospaceFontSize: 18
+    },
+  });
 
-	win = new BrowserWindow({
-		show: false,
-		frame: false,
-		// titleBarStyle: 'hidden',
-		// backgroundColor: "#fff",
-		height: 220,
-		// skipTaskbar: true,
-		// resizable: false,
-		// maxHeight: 190,
-		// width: 250,
-		// maxWidth: 510,
-		transparent: true,
-		webPreferences: {
-			nodeIntegration: true,
-			minimumFontSize: 16,
-			defaultFontSize: 18,
-			// defaultMonospaceFontSize: 18
-		}
-	})
+  // win = new BrowserWindow({
+  // 	show: false,
+  // 	frame: false,
+  // 	height: 250,
+  // 	width: 250,
+  // 	movable: false,
+  // 	center: true,
+  // 	kiosk: true,
+  // 	transparent: true,
+  // })
 
-	// win = new BrowserWindow({
-	// 	show: false,
-	// 	frame: false,
-	// 	height: 250,
-	// 	width: 250,
-	// 	movable: false,
-	// 	center: true,
-	// 	kiosk: true,
-	// 	transparent: true,
-	// })
+  //Delete clipboard history Start
+  // ipcMain.on('delete-message', (event, arg) => {
 
+  // 	console.log(" ID :: " + arg)
+  // 	var del = clipboardHistoryList.indexOf(arg);
+  // 	clipboardHistoryList.splice(del, 1)
 
+  // 	// clipboardRotate(clipboardHistoryList)
 
-	//Delete clipboard history Start
-	// ipcMain.on('delete-message', (event, arg) => {
+  // 	//  event.reply('asynchronous-reply', clipboardHistoryList);
+  // })
+  //Delete clipboard history End
 
-	// 	console.log(" ID :: " + arg)
-	// 	var del = clipboardHistoryList.indexOf(arg);
-	// 	clipboardHistoryList.splice(del, 1)
-      
-	// 	// clipboardRotate(clipboardHistoryList)
-		
-	// 	//  event.reply('asynchronous-reply', clipboardHistoryList);
-	// })
-	//Delete clipboard history End
+  // ClipboardWatcher  Start
+  clipboardWatcher({
+    // (optional) delay in ms between polls
+    watchDelay: 10,
+    onTextChange: function (text) {
+      console.log("copied....!");
+      console.log(text);
 
+      // clipboardHistoryList = clipboardHistoryList.map(item => {
+      //     item.isCurrent = false;
+      //     return item;
+      // })
 
+      // clipboardHistoryList.unshift({
+      //     value: text,
+      //     isCurrent: true
+      // });
 
-	// ClipboardWatcher  Start
-	clipboardWatcher({
-		// (optional) delay in ms between polls
-		watchDelay: 10,
-		onTextChange: function (text) {
+      if (clipboardHistoryList.length > clipboardMaxLength) {
+        clipboardHistoryList.splice(clipboardHistoryList.length - 1, 1);
 
-			console.log("copied....!");
-			console.log(text);
+        clipboardHistoryList.unshift({
+          id: randomstring.generate(7),
+          value: text,
+          isCurrent: true,
+          coppyAt: new Date(),
+        });
+      } else {
+        clipboardHistoryList.unshift({
+          id: randomstring.generate(7),
+          value: text,
+		  isCurrent: true,
+          coppyAt: new Date(),
+        });
+      }
+      // console.log(clipboardHistoryList);
+    },
+  });
+  //ClipboardWatcher  End
 
-			// clipboardHistoryList = clipboardHistoryList.map(item => {
-			//     item.isCurrent = false;
-			//     return item;
-			// })
+  //Iohook KEYUP START
 
-			// clipboardHistoryList.unshift({
-			//     value: text,
-			//     isCurrent: true
-			// });
+  ioHook.on("keyup", (event) => {
+    if (event.altKey && event.ctrlKey && event.keycode !== 47) {
+      console.log("closing..");
+      if (win) {
+        win.hide();
+      }
+    }
+  });
 
-			if (clipboardHistoryList.length > clipboardMaxLength) {
-				clipboardHistoryList.splice(clipboardHistoryList.length - 1, 1);
+  //Iohook KEYUP END
 
-				clipboardHistoryList.unshift({
-					id: randomstring.generate(7),
-					value: text,
-					isCurrent: true
-				});
-			} else {
-				clipboardHistoryList.unshift({
-					id: randomstring.generate(7),
-					value: text,
-					isCurrent: true
-				});
-			}
-			// console.log(clipboardHistoryList);
-		}
-	})
-	//ClipboardWatcher  End
+  //Iohook KEY DOWN START
+  ioHook.on("keydown", (event) => {
+    if (event.altKey && event.ctrlKey && event.keycode === 47) {
+      if (
+        clipboardHistoryList === undefined ||
+        clipboardHistoryList.length === 0
+      ) {
+        console.log("Clipbored Empty");
 
+        win.show();
+        // ipcMain.once('asynchronous-message', (event, arg) => {
+        // 	console.log(arg);
+        // 	event.reply('asynchronous-reply', 'empty');
+        // })
+      } else {
+        console.log("Rotating clipboard");
+        clipboardHistoryList = clipboardRotate(clipboardHistoryList);
 
-	//Iohook KEYUP START
+        setTimeout(() => {
+          if (clipboardHistoryList.length > 1) {
+            clipboardHistoryList.splice(0, 1);
+          }
 
-	ioHook.on('keyup', event => {
-		if (event.altKey && event.ctrlKey && event.keycode !== 47) {
-			console.log("closing..")
-			if (win) {
-				win.hide();
-			}
-		}
-	});
+          console.log("array rotated");
+          console.log("opening the window..");
+          if (win) {
+            win.show();
+            ipcMain.once("asynchronous-message", (event, arg) => {
+              console.log(arg);
 
-	//Iohook KEYUP END     
+              event.reply("asynchronous-reply", clipboardHistoryList);
+            });
 
-	//Iohook KEY DOWN START
-	ioHook.on('keydown', event => {
+            // win.webContents.openDevTools();
+            win.setVisibleOnAllWorkspaces(true);
+            win.loadFile("src/index.html");
+          } else {
+            win.show();
 
-		if (event.altKey && event.ctrlKey && event.keycode === 47) {
+            ipcMain.once("asynchronous-message", (event, arg) => {
+              console.log(arg);
 
-			if (clipboardHistoryList === undefined || clipboardHistoryList.length === 0) {
-				console.log("Clipbored Empty")
+              event.reply("asynchronous-reply", clipboardHistoryList);
+            });
 
-				win.show()
-				// ipcMain.once('asynchronous-message', (event, arg) => {
-				// 	console.log(arg);
-				// 	event.reply('asynchronous-reply', 'empty');
-				// })
+            // win.webContents.openDevTools();
+            win.setVisibleOnAllWorkspaces(true);
+            win.loadFile("src/index.html");
+          }
+        }, 15);
+      }
+    }
+  });
+  //Iohook KEY DOWN END
 
-			} else {
-				console.log("Rotating clipboard");
-				clipboardHistoryList = clipboardRotate(clipboardHistoryList);
+  ioHook.start();
+};
 
+app.on("ready", () => {
+  createWindow();
 
-				setTimeout(() => {
+  tray = new Tray("icon.png");
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Quit",
+      click: () => {
+        console.log("quiting..");
+        app.quit();
+      },
+    },
+  ]);
 
-					if (clipboardHistoryList.length > 1) {
-						clipboardHistoryList.splice(0, 1);
-					}
+  // tray.setToolTip('clipboard2')
+  tray.setContextMenu(contextMenu);
+});
 
-					console.log("array rotated")
-					console.log('opening the window..');
-					if (win) {
-						win.show();
-						ipcMain.once('asynchronous-message', (event, arg) => {
-							console.log(arg);
+app.on("window-all-closed", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
 
-							event.reply('asynchronous-reply', clipboardHistoryList);
-						})
-
-
-
-						// win.webContents.openDevTools();
-						win.setVisibleOnAllWorkspaces(true);
-						win.loadFile('src/index.html');
-					} else {
-						win.show()
-
-						ipcMain.once('asynchronous-message', (event, arg) => {
-							console.log(arg)
-
-							event.reply('asynchronous-reply', clipboardHistoryList);
-						})
-
-
-						// win.webContents.openDevTools();
-						win.setVisibleOnAllWorkspaces(true);
-						win.loadFile('src/index.html');
-					}
-				}, 15);
-			}
-		}
-	});
-	//Iohook KEY DOWN END
-
-	ioHook.start();
-}
-
-app.on('ready', () => {
-	createWindow();
-
-	tray = new Tray('icon.png');
-	const contextMenu = Menu.buildFromTemplate([
-    { label: 'Quit', click: () => { 
-			console.log("quiting..");
-			app.quit();
-		}},
-	]);
-	
-	// tray.setToolTip('clipboard2')
-	tray.setContextMenu(contextMenu);
-
-})
-
-
-
-app.on('window-all-closed', () => {
-	if (win === null) {
-		createWindow()
-	}
-})
-
-app.on('active', () => {
-	if (win == null) {
-		createWindow()
-	}
-})
+app.on("active", () => {
+  if (win == null) {
+    createWindow();
+  }
+});
--- src/renderer.js
+++ src/renderer.js
@@ -1,8 +1,7 @@
-const {
-	ipcRenderer
-} = require('electron');
+const { ipcRenderer } = require("electron");
+const { format, register } = require("timeago.js");
+//var ta = require('time-ago.js')
 // const tt = require('electron-tooltip')
-
 console.log("from renderer");
 // tt({
 // 	position: 'bottom',
@@ -13,52 +12,98 @@ console.log("from renderer");
 // 	}
 // })
 
-ipcRenderer.on('asynchronous-reply', (event, arg) => {
+const localeFunc = (number, index, totalSec) => {
+  // number: the timeago / timein number;
+  // index: the index of array below;
+  // totalSec: total seconds between date to be formatted and today's date;
+  return [
+    ['just now', 'right now'],
+    ['%s sec ago', 'in %s seconds'],
+    ['1 min ago', 'in 1 minute'],
+    ['%s mins ago', 'in %s minutes'],
+    ['1 hr ago  ', 'in 1 hour'],
+    ['%s hrs ago  ', 'in %s hours'],
+    ['1 day ago  ', 'in 1 day'],
+    ['%s days ago', 'in %s days'],
+    ['1 week ago', 'in 1 week'],
+    ['%s weeks ago', 'in %s weeks'],
+    ['1 month ago', 'in 1 month'],
+    ['%s months ago', 'in %s months'],
+    ['1 year ago', 'in 1 year'],
+    ['%s years ago', 'in %s years'],
+  ][index];
+};
+// register your locale with timeago
+register("my-locale", localeFunc);
 
-	console.log(arg)
-	var clipboard = document.getElementById("clipboard");
+// use it
 
-	// debugger;
-	if (arg === 'empty') {
-		// var clipboard = document.getElementById("clipboard");
-		// console.log("clipboard empty")
-		// clipboard.innerHTML = `
-		// 	<li>Your clipbored is empty !!!</li>
-		// `;
-	} else {
+ipcRenderer.on("asynchronous-reply", (event, arg) => {
+  console.log("aa=>", arg);
+  var clipboard = document.getElementById("clipboard");
 
-		console.log("rendering for first time..")
-		arg.forEach((item, i) => {
-			var li = document.createElement("li");
-			li.setAttribute("title", item.value);
-			if (item.value.length > 50) {
-				var res = item.value.substring(0, 50);
-				arg[i].value = res + " ....";
-			}
-			if (item.isCurrent === true) {
-				// clipboard.innerText += `<li id="${item.id}" class='current'>${item.value.trim()}</li>`;
-				var node = document.createTextNode(`${item.value.trim()}`)
-				li.classList.add("current");
+  // debugger;
+  if (arg === "empty") {
+    // var clipboard = document.getElementById("clipboard");
+    // console.log("clipboard empty")
+    // clipboard.innerHTML = `
+    // 	<li>Your clipbored is empty !!!</li>
+    // `;
+    // var li = document.createElement("div");
+    // li.innerText = "clipboard empty";
+    // clipboard.appendChild(li);
+    // console.log("rendering for first time..");
+  } else {
+    console.log("rendering for first time..");
+    arg.forEach((item, i) => {
+      var historyWrapper = document.createElement("div");
+      var historyDiv = document.createElement("div");
+      var timeDiv = document.createElement("div");
+      timeDiv.classList.add("time");
+      historyDiv.classList.add("history");
+      historyWrapper.classList.add("history__wrapper");
+      historyWrapper.setAttribute("title", item.value);
+      if (item.value.length > 60) {
+        var res = item.value.substring(0, 60);
+        arg[i].value = res + " ....";
+      }
+      if (item.isCurrent === true) {
+        // clipboard.innerText += `<li id="${item.id}" class='current'>${item.value.trim()}</li>`;
+        // var timeNode = document.createTextNode(`${}`);
+        //  var timeNode = document.createTextNode(`${timeago.format(item.coppyAt)}`);
 
-			} else {
-				// clipboard.innerText += `<li id="${item.id}">${item.value}</li>`;
-				var node = document.createTextNode(`${item.value.trim()}`);
-				// var btn = document.createElement('button');
-				// btn.className = 'icons';
-				// var text = document.createTextNode('delete');
-				// btn.onclick = () =>{
-					 
-				// ipcRenderer.send('delete-message', item.id)
+        var timeNode = document.createTextNode(
+          `${format(item.coppyAt, "my-locale")}`
+        );
+        var historyNode = document.createTextNode(`${item.value.trim()}`);
 
-				// 	};
-				// btn.appendChild(text);
-				// li.append(btn);
+        historyWrapper.classList.add("current");
+      } else {
+        // clipboard.innerText += `<li id="${item.id}">${item.value}</li>`;
+        // var timeNode = document.createTextNode(`${ta.ago(item.coppyAt, true)}`);
+        var timeNode = document.createTextNode(
+          `${format(item.coppyAt, "my-locale")}`
+        );
+        var historyNode = document.createTextNode(`${item.value.trim()}`);
 
-			}
-                li.append(node)
-				clipboard.appendChild(li);
-		});
-	}
+        // var btn = document.createElement('button');
+        // btn.className = 'icons';
+        // var text = document.createTextNode('delete');
+        // btn.onclick = () =>{
+
+        // ipcRenderer.send('delete-message', item.id)
+
+        // 	};
+        // btn.appendChild(text);
+      }
+      timeDiv.append(timeNode);
+      historyDiv.append(historyNode);
+      historyWrapper.append(timeDiv);
+      historyWrapper.append(historyDiv);
+
+      clipboard.appendChild(historyWrapper);
+    });
+  }
 });
 
-ipcRenderer.send('asynchronous-message', 'ping')
+ipcRenderer.send("asynchronous-message", "ping");
